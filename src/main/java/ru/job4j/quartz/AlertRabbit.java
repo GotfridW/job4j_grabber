f927f60d3c12be48d4211b71690697658f08be53
Gotfrid <gotfridwallenstein@gmail.com>
2022-12-07 14:55:16 +0300

    Corrections in AlertRabbit class

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -1,50 +1,52 @@
 package ru.job4j.quartz;
 
-import org.quartz.*;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Properties;
 
-import static org.quartz.JobBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
-import static org.quartz.TriggerBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
-    private static Properties getProperties() {
-        var properties = new Properties();
-        try (InputStream in = AlertRabbit.class.getClassLoader()
+    private static Properties getProperties() throws IOException {
+        try (var in = AlertRabbit.class.getClassLoader()
                 .getResourceAsStream("rabbit.properties")) {
+            var properties = new Properties();
             properties.load(in);
-        } catch (IOException e) {
-            e.printStackTrace();
+            return properties;
         }
-        return properties;
     }
 
     private static Connection getConnection(Properties properties) throws
             ClassNotFoundException, SQLException {
         Class.forName(properties.getProperty("driver"));
-        String url = properties.getProperty("url");
-        String login = properties.getProperty("login");
-        String password = properties.getProperty("password");
+        var url = properties.getProperty("url");
+        var login = properties.getProperty("login");
+        var password = properties.getProperty("password");
         return DriverManager.getConnection(url, login, password);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var properties = getProperties();
         try (var connection = getConnection(properties)) {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            var scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             var data = new JobDataMap();
             data.put("connection", connection);
-            JobDetail job = newJob(Rabbit.class)
+            var job = newJob(Rabbit.class)
                     .usingJobData(data)
                     .build();
-            int interval = Integer.parseInt(getProperties()
+            var interval = Integer.parseInt(properties
                     .getProperty("rabbit.interval"));
             var times = simpleSchedule()
                     .withIntervalInSeconds(interval)
@@ -57,9 +59,9 @@ public class AlertRabbit {
             Thread.sleep(10000);
             scheduler.shutdown();
             try (var statement = connection.createStatement()) {
-                ResultSet set = statement.executeQuery("select * from rabbit");
-                while (set.next()) {
-                    System.out.println(set.getString("created_date"));
+                var resultSet = statement.executeQuery("select * from rabbit");
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("created_date"));
                 }
             }
         } catch (Exception e) {
@@ -74,7 +76,7 @@ public class AlertRabbit {
         }
 
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
             var cn = (Connection) context.getJobDetail().getJobDataMap().get("connection");
             try (var ps = cn.prepareStatement("insert into rabbit(created_date) values (?)")) {
