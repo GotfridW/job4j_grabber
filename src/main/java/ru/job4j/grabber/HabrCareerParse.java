f48f18a193d9d55e9922a416342c0d63ddfdf9be
Gotfrid <gotfridwallenstein@gmail.com>
2022-12-09 01:10:46 +0300

    Task [#285213] - HabrCareerParse

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,15 +1,23 @@
 package ru.job4j.grabber;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     private String retrieveDescription(String link) throws IOException {
         var document = Jsoup.connect(link).get();
@@ -17,21 +25,34 @@ public class HabrCareerParse {
         return Objects.requireNonNull(descriptionElement).text();
     }
 
-    public static void main(String[] args) throws IOException {
-        for (var i = 1; i <= 5; i++) {
-            var connection = Jsoup.connect(PAGE_LINK + i);
-            var document = connection.get();
-            var rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                var titleElement = row.select(".vacancy-card__title").first();
-                var linkElement = Objects.requireNonNull(titleElement).child(0);
-                var dateElement = row.select(".basic-date");
-                var vacancyName = titleElement.text();
-                var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                var dateAttr = dateElement.attr("datetime");
-                var dateTime = LocalDateTime.parse(dateAttr, DateTimeFormatter.ISO_DATE_TIME);
-                System.out.printf("%s %s %s %n", dateTime, vacancyName, link);
-            });
+    private Post createPost(Element element) throws IOException {
+        var linkElement = element.select(".vacancy-card__title").first();
+        var dateElement = element.select(".basic-date");
+        var title = Objects.requireNonNull(linkElement).child(0).text();
+        var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        var description = retrieveDescription(link);
+        var created = dateTimeParser.parse(dateElement.attr("datetime"));
+        return new Post(title, link, description, created);
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> postList = new ArrayList<>();
+        try {
+            for (var i = 1; i <= 5; i++) {
+                var document = Jsoup.connect(link).get();
+                var cards = document.select(".vacancy-card__inner");
+                cards.forEach(card -> {
+                    try {
+                        postList.add(createPost(card));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return postList;
     }
 }
