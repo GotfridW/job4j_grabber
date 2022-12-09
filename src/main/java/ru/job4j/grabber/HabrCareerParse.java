ea96cc11bfcacd5b7f0937e6050b117c4439d88e
Gotfrid <gotfridwallenstein@gmail.com>
2022-12-09 21:24:12 +0300

    Refactored methods retrieveDescription & list; field PAGES added

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,6 +1,7 @@
 package ru.job4j.grabber;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import ru.job4j.grabber.utils.DateTimeParser;
 
@@ -13,19 +14,25 @@ import java.util.Objects;
 public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private static final int PAGES = 5;
     private final DateTimeParser dateTimeParser;
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
     }
 
-    private String retrieveDescription(String link) throws IOException {
-        var document = Jsoup.connect(link).get();
-        var descriptionElement = document.selectFirst(".style-ugc");
-        return Objects.requireNonNull(descriptionElement).text();
+    private String retrieveDescription(String link) {
+        Document document;
+        try {
+            document = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            throw new IllegalArgumentException(String.format(
+                    "Error getting description from %s", link));
+        }
+        return Objects.requireNonNull(document.selectFirst(".style-ugc")).text();
     }
 
-    private Post createPost(Element element) throws IOException {
+    private Post createPost(Element element) {
         var linkElement = element.select(".vacancy-card__title").first();
         var dateElement = element.select(".basic-date");
         var title = Objects.requireNonNull(linkElement).child(0).text();
@@ -39,19 +46,15 @@ public class HabrCareerParse implements Parse {
     public List<Post> list(String link) {
         List<Post> postList = new ArrayList<>();
         try {
-            for (var i = 1; i <= 5; i++) {
-                var document = Jsoup.connect(link).get();
+            for (var i = 1; i <= PAGES; i++) {
+                var document = Jsoup.connect(link + i).get();
                 var cards = document.select(".vacancy-card__inner");
-                cards.forEach(card -> {
-                    try {
-                        postList.add(createPost(card));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
+                cards.stream()
+                        .map(this::createPost)
+                        .forEach(postList::add);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new IllegalArgumentException("Error getting document");
         }
         return postList;
     }
